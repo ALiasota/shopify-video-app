48399de5a2a585da3dd50879b5d0ab0e582786c2
Andrii <liasotaandrii0@gmail.com>
2025-07-25 14:56:56 +0300

    update database

--- drizzle/schema.server.ts
+++ drizzle/schema.server.ts
@@ -6,7 +6,8 @@ export const merchantTable = pgTable("merchant", {
     shop: text("shop").notNull().unique(),
     shopId: text("shop_id").notNull(),
     email: text("email").notNull(),
-    createdAt: timestamp("created_at").defaultNow(),
+    createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
+    updatedAt: timestamp("updated_at", { mode: "date" }),
 });
 
 export const sessionTable = pgTable("session", {
@@ -23,14 +24,15 @@ export const videoTable = pgTable("video", {
     merchantId: integer("merchant_id")
         .references(() => merchantTable.id, { onDelete: "cascade" })
         .notNull(),
-    videoId: text("video_id").notNull(),
-    thumbnailUrl: text("shop").notNull(),
-    videoUrl: text("shop").notNull(),
-    filename: text("shop").notNull(),
-    duration: text("shop").notNull(),
-    format: text("shop").notNull(),
-    size: text("shop").notNull(),
-    createdAt: timestamp("created_at").defaultNow(),
+    shopifyVideoId: text("shopify_video_id").notNull(),
+    thumbnailUrl: text("thumbnail_url").notNull(),
+    videoUrl: text("video_url").notNull(),
+    filename: text("filename").notNull(),
+    duration: text("duration").notNull(),
+    format: text("format").notNull(),
+    size: text("size").notNull(),
+    createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
+    updatedAt: timestamp("updated_at", { mode: "date" }),
 });
 
 export const videoLayoutTypeEnum = pgEnum("video_layout_type", ["carousel", "stack", "single", "thumbnails"]);
@@ -40,16 +42,23 @@ export const videoSliderTable = pgTable("video_slider", {
     merchantId: integer("merchant_id")
         .references(() => merchantTable.id, { onDelete: "cascade" })
         .notNull(),
-    title: text("video_id").notNull(),
+    title: text("title").notNull(),
     layout: videoLayoutTypeEnum("layout").notNull(),
-    createdAt: timestamp("created_at").defaultNow(),
+    createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
+    updatedAt: timestamp("updated_at", { mode: "date" }),
 });
 
 export const videoSliderItemTable = pgTable("video_slider_item", {
     id: text("id").primaryKey(),
-    title: text("video_id").notNull(),
-    layout: videoLayoutTypeEnum("layout").notNull(),
-    createdAt: timestamp("created_at").defaultNow(),
+    sliderId: integer("slider_id")
+        .references(() => videoSliderTable.id, { onDelete: "cascade" })
+        .notNull(),
+    videoId: integer("video_id")
+        .references(() => videoTable.id, { onDelete: "cascade" })
+        .notNull(),
+    shopifyVideoId: text("shopify_product_id").notNull(),
+    createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
+    updatedAt: timestamp("updated_at", { mode: "date" }),
 });
 
 export const merchantTableRelations = relations(merchantTable, ({ many }) => ({
@@ -57,17 +66,30 @@ export const merchantTableRelations = relations(merchantTable, ({ many }) => ({
     sliders: many(videoSliderTable),
 }));
 
-export const videoTableRelations = relations(videoTable, ({ one }) => ({
+export const videoTableRelations = relations(videoTable, ({ one, many }) => ({
     merchant: one(merchantTable, {
         fields: [videoTable.merchantId],
         references: [merchantTable.id],
     }),
+    sliderItems: many(videoSliderItemTable),
 }));
 
-export const videoSliderTableRelations = relations(videoSliderTable, ({ one }) => ({
+export const videoSliderTableRelations = relations(videoSliderTable, ({ one, many }) => ({
     merchant: one(merchantTable, {
         fields: [videoSliderTable.merchantId],
         references: [merchantTable.id],
+    }),
+    sliderItems: many(videoSliderItemTable),
+}));
+
+export const videoSliderItemTableRelations = relations(videoSliderItemTable, ({ one }) => ({
+    slider: one(videoSliderTable, {
+        fields: [videoSliderItemTable.sliderId],
+        references: [videoSliderTable.id],
+    }),
+    video: one(videoTable, {
+        fields: [videoSliderItemTable.videoId],
+        references: [videoTable.id],
     }),
 }));
 
